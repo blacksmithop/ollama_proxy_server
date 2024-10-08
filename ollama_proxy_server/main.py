dc5f583c9a19b62135f05aeef5242ca61ef46515
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-10-08 14:52:06 +0530

    Update main.py
    Removed authentication for use in isolated environment

--- ollama_proxy_server/main.py
+++ ollama_proxy_server/main.py
@@ -23,22 +23,6 @@ def get_config(filename):
     config.read(filename)
     return [(name, {'url': config[name]['url'], 'queue': Queue()}) for name in config.sections()]
 
-# Read the authorized users and their keys from a file
-def get_authorized_users(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-    authorized_users = {}
-    for line in lines:
-        if line=="":
-            continue
-        try:
-            user, key = line.strip().split(':')
-            authorized_users[user] = key
-        except:
-            ASCIIColors.red(f"User entry broken:{line.strip()}")
-    return authorized_users
-
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,20 +33,12 @@ def main():
     parser.add_argument('-d', '--deactivate_security', action='store_true', help='Deactivates security')
     args = parser.parse_args()
     servers = get_config(args.config)  
-    authorized_users = get_authorized_users(args.users_list)
-    deactivate_security = args.deactivate_security
     ASCIIColors.red("Ollama Proxy server")
     ASCIIColors.red("Author: ParisNeo")
 
     class RequestHandler(BaseHTTPRequestHandler):
         def add_access_log_entry(self, event, user, ip_address, access, server, nb_queued_requests_on_server, error=""):
             log_file_path = Path(args.log_path)
-        
-            if not log_file_path.exists():
-                with open(log_file_path, mode='w', newline='') as csvfile:
-                    fieldnames = ['time_stamp', 'event', 'user_name', 'ip_address', 'access', 'server', 'nb_queued_requests_on_server', 'error']
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writeheader()
         
             with open(log_file_path, mode='a', newline='') as csvfile:
                 fieldnames = ['time_stamp', 'event', 'user_name', 'ip_address', 'access', 'server', 'nb_queued_requests_on_server', 'error']
@@ -95,40 +71,9 @@ def main():
             self.log_request()
             self.proxy()
 
-        def _validate_user_and_key(self):
-            try:
-                # Extract the bearer token from the headers
-                auth_header = self.headers.get('Authorization')
-                if not auth_header or not auth_header.startswith('Bearer '):
-                    return False
-                token       = auth_header.split(' ')[1]
-                user, key   = token.split(':')
-                
-                # Check if the user and key are in the list of authorized users
-                if authorized_users.get(user) == key:
-                    self.user = user
-                    return True
-                else:
-                    self.user = "unknown"
-                return False
-            except:
-                return False
-                
+       
         def proxy(self):
             self.user = "unknown"
-            if not deactivate_security and not self._validate_user_and_key():
-                ASCIIColors.red(f'User is not authorized')
-                client_ip, client_port = self.client_address
-                # Extract the bearer token from the headers
-                auth_header = self.headers.get('Authorization')
-                if not auth_header or not auth_header.startswith('Bearer '):
-                    self.add_access_log_entry(event='rejected', user="unknown", ip_address=client_ip, access="Denied", server="None", nb_queued_requests_on_server=-1, error="Authentication failed")
-                else:
-                    token = auth_header.split(' ')[1]                
-                    self.add_access_log_entry(event='rejected', user=token, ip_address=client_ip, access="Denied", server="None", nb_queued_requests_on_server=-1, error="Authentication failed")
-                self.send_response(403)
-                self.end_headers()
-                return            
             url = urlparse(self.path)
             path = url.path
             get_params = parse_qs(url.query) or {}
@@ -163,6 +108,7 @@ def main():
                         post_data_dict = json.loads(post_data_str)
 
                     response = requests.request(self.command, min_queued_server[1]['url'] + path, params=get_params, data=post_params, stream=post_data_dict.get("stream", False))
+                    
                     self._send_response(response)
                 except Exception as ex:
                     self.add_access_log_entry(event="gen_error",user=self.user, ip_address=client_ip, access="Authorized", server=min_queued_server[0], nb_queued_requests_on_server=que.qsize(),error=ex)                    
